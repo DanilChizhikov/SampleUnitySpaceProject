707e7742b7c36da3649028f60a9a50ee1adf4f6e
Đanil Miller <clichindustries@gmail.com>
2021-03-03 18:31:05 +0700

    Add PowerOlant
    Add first model PowerPlant

---
+++ Assets/Data/Script/Preview/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Data.Script.Preview
+{
+    public class Rotate : MonoBehaviour
+    {
+        [SerializeField] private Vector3 rotationVector;
+        [SerializeField] private float multiplaySpeed;
+
+        private void Update()
+        {
+            var euler = rotationVector * multiplaySpeed * Time.deltaTime;
+            transform.Rotate(euler);
+        }
+    }
+}
